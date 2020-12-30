cfc5386037f548dbae25d871a601837856b4ff84
Carrie Schmid <carriepederson7@gmail.com>
2020-12-30 15:49:53 -0800

    Add Toolbar, SideDrawer and NavigationItems

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    //.Close and .Open for playing animation
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                {/* we can't pass an array of string, must pass a single string, that's the reason for .join('')  */}
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+};
+
+export default sideDrawer;
